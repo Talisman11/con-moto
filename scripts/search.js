92c7dba07a23730260e7d90f6b03e22b1ac97537
AvaniPai <avanipai695@gmail.com>
2016-02-20 03:34:45 -0600

    gulp

---
+++ scripts/search.js
@@ -0,0 +1,24 @@
+function showResponse(response) {
+	var responseString = JSON.stringify(response, '', 2);
+	document.getElementById('response').innerHTML('response')+=responseString;
+}
+
+function onClientLoad() {
+	gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+function onYouTubeApiLoad() {
+	gapi.client.setApiKey('AIzaSyDP2Ia9uuC18HBIs8MloJe6fzMKNs5UtNg');
+	search();
+}
+
+function search(){
+	var request = gapi.client.youtube.search.list({
+		part: 'snippet'
+	});
+	request.execute(onSearchResponse);
+}
+
+function onSearchResponse(response){
+	showResponse(response);
+}
