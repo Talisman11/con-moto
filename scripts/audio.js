fea07b31b57acf662b75d5524557651076b66279
unFearing <chen240sx@gmail.com>
2016-02-20 12:53:59 -0600

    canvas update?

--- scripts/audio.js
+++ scripts/audio.js
@@ -20,7 +20,7 @@ var ctx = $("#canvas").get()[0].getContext("2d");
 // create a gradient for the fill. Note the strange
 // offset, since the gradient is calculated based on
 // the canvas, not the specific element we draw
-var gradient = ctx.createLinearGradient(0,0,0,130);
+var gradient = ctx.createLinearGradient(0,0,0,900);
 gradient.addColorStop(1,'#000000');
 gradient.addColorStop(0.75,'#ff0000');
 gradient.addColorStop(0.25,'#ffff00');
@@ -40,7 +40,7 @@ function setupAudioNodes() {
     // setup a analyzer
     analyser = context.createAnalyser();
     analyser.smoothingTimeConstant = 0.3;
-    analyser.fftSize = 512;
+    analyser.fftSize = 1024;
 
     // create a buffer source node
     sourceNode = context.createBufferSource();
@@ -66,6 +66,31 @@ function loadSound(url) {
         }, onError);
     }
     request.send();
+
+    // var xhr = createCORSRequest('GET', url);
+
+    // if (!xhr) {
+    //   throw new Error('CORS not supported');
+    // }
+
+    // xhr.onload = function() {
+    //     xhr.addHeader("Access-Control-Allow-Origin", "*");
+
+    //     var responseText = xhr.responseText;
+    //     console.log(responseText);
+    //     // process the response.
+    //     context.decodeAudioData(xhr.response, function(buffer) {
+    //         playSound(buffer);
+    //     });
+
+    // };
+
+    // xhr.onerror = function() {
+    //   console.log('There was an error!');
+    // };
+
+    // xhr.send();
+
 }
 
 function playSound(buffer) {
@@ -88,7 +113,7 @@ javascriptNode.onaudioprocess = function() {
     analyser.getByteFrequencyData(array);
 
     // clear the current state
-    ctx.clearRect(0, 0, 1000, 325);
+    ctx.clearRect(0, 0, 900, 550);
 
     // set the fill style
     ctx.fillStyle=gradient;
@@ -100,7 +125,79 @@ function drawSpectrum(array) {
     for ( var i = 0; i < (array.length); i++ ){
         var value = array[i];
 
-        ctx.fillRect(i*5,325-value,3,325);
-        //  console.log([i,value])
+        ctx.fillRect(i*5,550-value,3,300);
+        // console.log([i,value])
     }
 };
+// ------------------------
+
+// var FilterSample = {
+//   FREQ_MUL: 7000,
+//   QUAL_MUL: 30,
+//   playing: false
+// };
+
+// FilterSample.play = function() {
+//   // Create the source.
+//   var source = context.createBufferSource();
+//   source.buffer = BUFFERS.techno;
+//   // Create the filter.
+//   var filter = context.createBiquadFilter();
+//   //filter.type is defined as string type in the latest API. But this is defined as number type in old API.
+//   filter.type = (typeof filter.type === 'string') ? 'lowpass' : 0; // LOWPASS
+//   filter.frequency.value = 5000;
+//   // Connect source to filter, filter to destination.
+//   source.connect(filter);
+//   filter.connect(context.destination);
+//   // Play!
+//   if (!source.start)
+//     source.start = source.noteOn;
+//   source.start(0);
+//   source.loop = true;
+//   // Save source and filterNode for later access.
+//   this.source = source;
+//   this.filter = filter;
+// };
+
+// FilterSample.stop = function() {
+//   if (!this.source.stop)
+//     this.source.stop = source.noteOff;
+//   this.source.stop(0);
+//   this.source.noteOff(0);
+// };
+
+// FilterSample.toggle = function() {
+//   this.playing ? this.stop() : this.play();
+//   this.playing = !this.playing;
+// };
+
+// FilterSample.changeFrequency = function(element) {
+//   // Clamp the frequency between the minimum value (40 Hz) and half of the
+//   // sampling rate.
+//   var minValue = 40;
+//   var maxValue = context.sampleRate / 2;
+//   // Logarithm (base 2) to compute how many octaves fall in the range.
+//   var numberOfOctaves = Math.log(maxValue / minValue) / Math.LN2;
+//   // Compute a multiplier from 0 to 1 based on an exponential scale.
+//   var multiplier = Math.pow(2, numberOfOctaves * (element.value - 1.0));
+//   // Get back to the frequency value between min and max.
+//   this.filter.frequency.value = maxValue * multiplier;
+// };
+
+// FilterSample.changeQuality = function(element) {
+//   this.filter.Q.value = element.value * this.QUAL_MUL;
+// };
+
+// FilterSample.toggleFilter = function(element) {
+//   this.source.disconnect(0);
+//   this.filter.disconnect(0);
+//   // Check if we want to enable the filter.
+//   if (element.checked) {
+//     // Connect through the filter.
+//     this.source.connect(this.filter);
+//     this.filter.connect(context.destination);
+//   } else {
+//     // Otherwise, connect directly.
+//     this.source.connect(context.destination);
+//   }
+// };
